96c487215753600f103f008a08ff878a5a0bc730
Jakob Gross <jakobgross@gmail.com>
2023-12-05 09:39:20 +0100

    form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
-
+import "./App.css";
+import { Form } from "./components/form/Form";
 function App() {
   return (
     <>
       <h1>main app</h1>
+      <Form />
     </>
   );
 }
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,3 +1,56 @@
-export const Form = () => {
-  return <div>Form</div>;
+import { useState } from "react";
+export const Form = ({ onAddActivity }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    isForGoodWeather: false,
+  });
+
+  // const inputFocus = useRef(null);
+
+  const handleNameChange = (e) => {
+    setFormData({ ...formData, name: e.target.value });
+  };
+  const handleWeatherChange = (e) => {
+    setFormData({ ...formData, isForGoodWeather: e.target.checked });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("target", e.target);
+    console.log("form", formData);
+
+    onAddActivity({ formData });
+
+    // if (inputFocus.current) {
+    //   inputFocus.current.focus();
+    // }
+    // setFormData({
+    //   name: "",
+    //   isForGoodWeather: false,
+    // })
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <h4>Add new Activity:</h4>
+        <label htmlFor="inputName">Name:</label>
+        <input
+          type="text"
+          id="inputName"
+          placeholder="enter your activity"
+          value={formData.name}
+          onChange={handleNameChange}
+          // ref={inputFocus}
+        ></input>
+        <label htmlFor="inputGoodWeather">Good-weather activity:</label>
+        <input
+          type="checkbox"
+          id="inputGoodWeather"
+          onChange={handleWeatherChange}
+          checked={formData.isForGoodWeather}
+        ></input>
+        <input type="submit" value="add Activity"></input>
+      </form>
+    </>
+  );
 };
